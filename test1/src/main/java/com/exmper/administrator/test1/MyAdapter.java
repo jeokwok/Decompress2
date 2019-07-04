edf6313ee687bc456ae6b0ce035bcc1074c33e24
jeokwok <42006908+jeokwok@users.noreply.github.com>
2019-07-04 13:33:31 +0800

    Update MyAdapter.java

--- test1/src/main/java/com/exmper/administrator/test1/MyAdapter.java
+++ test1/src/main/java/com/exmper/administrator/test1/MyAdapter.java
@@ -43,39 +43,25 @@ public class MyAdapter extends BaseAdapter{
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         
-          ViewHolder holder = null;
-    if(convertView == null){
-        convertView = LayoutInflater.from(mContext).inflate(R.layout.item_list_animal,parent,false);
-        holder = new ViewHolder();
-        holder.img_icon = (ImageView) convertView.findViewById(R.id.img_icon);
-        holder.txt_aName = (TextView) convertView.findViewById(R.id.txt_aName);
-        holder.txt_aSpeak = (TextView) convertView.findViewById(R.id.txt_aSpeak);
-        convertView.setTag(holder);   //将Holder存储到convertView中
-    }else{
-        holder = (ViewHolder) convertView.getTag();
-    }
-    holder.img_icon.setBackgroundResource(mData.get(position).getaIcon());
-    holder.txt_aName.setText(mData.get(position).getaName());
-    holder.txt_aSpeak.setText(mData.get(position).getaSpeak());
-    return convertView;
-        
-        
-        
+        ViewHolder holder = null;
         
         if(converView == null){
            View view = layoutInflater.inflate(R.layout.custom_item1,parent, false);
-            mViewHolder = new ViewHolder();
-            TextView textView = (TextView)view .findViewById(R.id.tv1)
-            mViewHolder.textView = textView.getTag();
+            holder = new ViewHolder();
+            holder.textView = (TextView)view .findViewById(R.id.tv1)
+            convertView.setTag(holder);//将Holder存储到convertView中
+        }else{
+            
+            holder = (ViewHolder) convertView.getTag();
         }
 
+        holder.textView.setText(mData.get(position).getmName())
         //TextView textView = (TextView) view.findViewById(R.id.tv1);
-
-        User user =getItem(position);
+        //User user =getItem(position);
        // textView.setText(user.getmName());
-        mViewHolder.textView.setText(user.getmName());
+        //mViewHolder.textView.setText(user.getmName());
 
-        return converView;
+        return convertView;
     }
 }
 
